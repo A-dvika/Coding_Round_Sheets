3c0eecda48c670a9185bff78a5ec5772df7defb1
Advika Thakur <115079077+A-dvika@users.noreply.github.com>
2024-02-17 18:33:01 +0530

    Create Pascsal_Triangle.cpp

---
+++ Pascsal_Triangle.cpp
@@ -0,0 +1,62 @@
+//Optimised
+class Solution {
+public:
+
+vector<int> generateRow(int row) {
+    long long ans = 1;
+    vector<int> ansRow;
+    ansRow.push_back(1); //inserting the 1st element
+
+    //calculate the rest of the elements:
+    for (int col = 1; col < row; col++) {
+        ans = ans * (row - col);
+        ans = ans / col;
+        ansRow.push_back(ans);
+    }
+    return ansRow;
+}
+    vector<vector<int>> generate(int numRows) {
+        int n=numRows;
+        vector<vector<int>> ans;
+
+    //store the entire pascal's triangle:
+    for (int row = 1; row <= n; row++) {
+        ans.push_back(generateRow(row));
+    }
+    return ans;}
+
+};
+
+
+
+//Brute Force
+class Solution {
+public:
+   int nCr(int n, int r) {
+    long long res = 1;
+
+    // calculating nCr:
+    for (int i = 0; i < r; i++) {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return (int)(res);
+}
+
+
+    vector<vector<int>> generate(int numRows) {
+         vector<vector<int>> ans;
+         int n=numRows;
+
+    //Store the entire pascal's triangle:
+    for (int row = 1; row <= n; row++) {
+        vector<int> tempLst; // temporary list
+        for (int col = 1; col <= row; col++) {
+            tempLst.push_back(nCr(row - 1, col - 1));
+        }
+        ans.push_back(tempLst);
+    }
+    return ans;
+    }
+};
+  
